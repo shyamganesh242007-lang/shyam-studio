426e8245825a9b014e855dd50c67222010eaf67b
S. Shyamganesh <shyamganesh242007@gmail.com>
2026-08-17 16:04:34 +0530

    Fix GitHub Pages routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import ContactAdmin from "./pages/ContactAdmin";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
